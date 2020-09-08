8bcdf9c8ba660925913060a46a26bcc0b752006e
josephmabe <34747098+josephmabe@users.noreply.github.com>
2020-09-08 08:55:40 +0100

    tweaks

--- demo/src/main/java/com/example/demo/DatabaseQuery.java
+++ demo/src/main/java/com/example/demo/DatabaseQuery.java
@@ -1,62 +1,29 @@
 package com.example.demo;
 
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-//
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import javax.sql.DataSource;
-//
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 import java.util.Collections;
 import java.util.Map;
 
+@RestController
 interface Query {
-    /*List<Phone> findAll();
 
     List<Phone> findByMakeOrModel(String searchTerm);
-    */
-    List<Phone> countRow(String make);
-};
+}
 
 class DatabaseQuery implements Query{
 
     @Override
-    public List<Phone> countRow(String make) {
-
+    public List<Phone> findByMakeOrModel(String searchTerm) {
 
         JdbcTemplateFactory Factory = new JdbcTemplateFactory();
         NamedParameterJdbcTemplate template = Factory.getTemplate();
 
-        String sql = "select * from Phone where make = :make";
-        Map namedParameters = Collections.singletonMap("make", make);
+        String sql = "select * from Phone where make = :searchTerm OR model =:searchTerm";
+        Map namedParameters = Collections.singletonMap("searchTerm", searchTerm);
         return template.queryForList(sql, namedParameters);
     }
-    /*
-    @Override
-    public List<Phone> findByMakeOrModel(String searchTerm){
-        String sqlQuery = "SELECT p from Phone p WHERE " +
-                "LOWER(p.make) LIKE LOWER(CONCAT('%',:searchTerm, '%')) OR " +
-                "LOWER(p.model) LIKE LOWER(CONCAT('%',:searchTerm, '%'))";
-
-        JdbcTemplateFactory Factory = new JdbcTemplateFactory();
-        NamedParameterJdbcTemplate template = Factory.getTemplate();
-
-        MapSqlParameterSource params = new MapSqlParameterSource();
-        params.addValue("searchTerm", searchTerm);
-        return template.query(sqlQuery, params);
-    }
-
-    @Override
-    public List<Phone> findAll() {
-        String sqlQuery = "SELECT * FROM Phone p";
-
-        JdbcTemplateFactory Factory = new JdbcTemplateFactory();
-        NamedParameterJdbcTemplate template = Factory.getTemplate();
-
-        return template.query(sqlQuery, new RowMapper<Phone>)
-     }
-     */
 }
